37d5ae9b9448216973c0374e9de2c5872571ea50
jhonatan <jhonatansouza@outlook.com>
2019-04-04 20:51:10 -0300

    [New Format] Alterando formado das funcoes que executa o bot

--- bot.js
+++ bot.js
@@ -9,56 +9,74 @@ const telegram = new Telegraf(env.token)
 const brazil = new Brazil()
 
 class Bot {
-    constructor(){
-        this.keyboardInitial = Markup.keyboard([['BTC Bitstamp'], ['Brasil Exchanges']]).resize().extra()
-        this.bitcoinControl  = {}
-    }
-    
-    setLastTransactionValue = (value) => {
-        this.bitcoinControl.lastValue = value
-    }
+  constructor(){
+      this.bitcoinControl  = {}
 
-    // telegram.start(ctx => {
-    //     const from = ctx.update.message.from
+      this.startBot()
+      this.messagesDefault()
+  }
 
-    //     if(from.id != env.id){
-    //         ctx.reply(`Sinto muito mais não falo com vc!, ${from.first_name}!`)
-    //         return;
-    //     }
+  keyboardInitial () {
+    return Markup.keyboard([['BTC Bitstamp'], ['Brasil Exchanges']]).resize().extra()
+  }
 
-    //     ctx.reply(
-    //         `Ao seu dispor, mano loko!!, ${from.first_name}!\nO que vc quer hoje meu consagrado?`,
-    //         keyboardInitial()
-    //     )
-    // })
+  setLastTransactionValue (value) {
+    this.bitcoinControl.lastValue = value
+  }
 
-    // telegram.hears('BTC Bitstamp', ctx => {
-    //     ctx.reply(`Valor da última transação na Bitstamp: ${bitcoinControl.lastValue}`, keyboardInitial())
-    // })
+  sendMessages (message) {
+    telegram.sendMessage(
+      env.id,
+      message,
+      this.keyboardInitial()
+    )
+  }
 
-    // telegram.hears('Brasil Exchanges', ctx => {
-    //     ctx.reply('Selecione Alguma Exchange no teclado de opções!', brazil.keyboardBrazilExchanges())
-    // })
+  startBot () {
+    telegram.start(ctx => {
+      const from = ctx.update.message.from
 
-    // telegram.hears(brazil.exchanges, async ctx => {
-    //     try{
-    //         const res = await brazil.getBasicDataFromExchange(ctx.match)
-    //         ctx.replyWithHTML(`Ultimos Valores na Exchange ${ctx.match}: \n\n Compra: ${res.BuyPrice} \n Venda: ${res.SellPrice} \n Ultima Transação: <code>${res.Last}</code>\n Volume: ${res.Vol} \n <pre>Variação: ${res.LastVariation}% </pre>`,
-    //         brazil.keyboardBrazilExchanges()
-    //         )
-    //     } catch (error){
-    //         ctx.reply(`A api da exchange ${ctx.match} está temporarimante indisponivel tente novamente mais tarde`)
-    //     }
-    // })
+      if(from.id != env.id){
+          ctx.reply(`Sinto muito mais não falo com vc!, ${from.first_name}!`)
+          return;
+      }
 
-    // telegram.hears('< Voltar', ctx => {
-    //     ctx.reply(
-    //         'Escolha Entre as opções do Teclado',
-    //         keyboardInitial()
-    //     )
-    // })
+      ctx.reply(
+          `Ao seu dispor, mano loko!!, ${from.first_name}!\nO que vc quer hoje meu consagrado?`,
+          this.keyboardInitial()
+      )
+    })
 
-    // telegram.startPolling()
+    telegram.startPolling()
+  }
+
+  messagesDefault () {
+    telegram.hears('BTC Bitstamp', ctx => {
+        ctx.reply(`Valor da última transação na Bitstamp: ${this.bitcoinControl.lastValue}`, this.keyboardInitial())
+    })
+
+    telegram.hears('Brasil Exchanges', ctx => {
+        ctx.reply('Selecione Alguma Exchange no teclado de opções!', brazil.keyboardBrazilExchanges())
+    })
+
+    telegram.hears(brazil.exchanges, async ctx => {
+      try{
+          const res = await brazil.getBasicDataFromExchange(ctx.match)
+          ctx.replyWithHTML(`Ultimos Valores na Exchange ${ctx.match}: \n\n Compra: ${res.BuyPrice} \n Venda: ${res.SellPrice} \n Ultima Transação: <code>${res.Last}</code>\n Volume: ${res.Vol} \n <pre>Variação: ${res.LastVariation}% </pre>`,
+          brazil.keyboardBrazilExchanges()
+          )
+      } catch (error){
+          ctx.reply(`A api da exchange ${ctx.match} está temporarimante indisponivel tente novamente mais tarde`)
+      }
+    })
+
+    telegram.hears('< Voltar', ctx => {
+        ctx.reply(
+            'Escolha Entre as opções do Teclado',
+            this.keyboardInitial()
+        )
+    })
+  }
 }
 
-module.exports = bot
+module.exports = Bot
--- index.js
+++ index.js
@@ -1,33 +1,30 @@
 const axios = require('axios')
-const robot = require('./bot.js')
+const Robot = require('./bot.js')
 
 const controlBTC = {}
 controlBTC.bitstamp = {}
 controlBTC.bitstamp.unit = ''
 
-robot();
+bot = new Robot();
 
 setInterval(async () => {
-    try{
-        const res  = await axios.get('https://www.bitstamp.net/api/ticker/')
-        const unit = /\d/.exec(/\d{3}[.]/.exec(res.data.last)[0])[0]
-        
-        controlBTC.bitstamp.value = res.data.last
+  try{
+    const res  = await axios.get('https://www.bitstamp.net/api/ticker/')
+    const unit = /\d/.exec(/\d{1}[.]/.exec(res.data.last)[0])[0]
 
-        robot = setLastTransactionValue( controlBTC.bitstamp.value )
+    controlBTC.bitstamp.value = res.data.last
 
-        if( controlBTC.bitstamp.unit == unit ){
-            return
-        }
-        // bot.telegram.sendMessage(
-        //     env.id, 
-        //     `BTC Saiu da casa ${controlBTC.bitstamp.unit}00 \n Agora está na casa dos ${unit}00: \n valor atual ${res.data.last}`,
-        //     keyboardInitial()
-        // )
+    bot.setLastTransactionValue(controlBTC.bitstamp.value)
 
-        // controlBTC.bitstamp.unit = unit
-    } catch (error){
-        console.log('error')
+    if( controlBTC.bitstamp.unit == unit ){
         return
     }
-}, 4000)
+
+    bot.sendMessages(`BTC Saiu da casa ${controlBTC.bitstamp.unit}00 \n Agora está na casa dos ${unit}00: \n valor atual ${res.data.last}`)
+
+    controlBTC.bitstamp.unit = unit
+  } catch (error){
+    console.log(error)
+    return
+  }
+}, 8000)
